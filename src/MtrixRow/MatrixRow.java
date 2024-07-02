227185b619097eca699e9437074ba785ce55cfa7
rytjr <kks007159@naver.com>
2024-07-02 13:52:59 +0900

    matrix

---
+++ src/MtrixRow/MatrixRow.java
@@ -0,0 +1,26 @@
+package MtrixRow;
+
+import java.util.*;
+
+public class MatrixRow {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		char[][] list = new char[5][15];
+		
+		for(int i = 0; i < 5; i++) {
+			String str = sc.nextLine();
+			for(int j = 0; j < str.length(); j++) {
+				list[i][j] = str.charAt(j);
+			}
+		}
+		for(int i = 0; i < 15; i++) {
+			for(int j = 0; j < 5; j++) {
+				if(list[j][i] != 0) {
+					System.out.print(list[j][i]);
+				}
+			}
+		}
+	}
+}
